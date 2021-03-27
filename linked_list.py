45e7056e7295c8c11ebe7976e590dc25d8366aff
Georgio Tunson <georgio@wealthpark.loc>
2021-03-27 15:12:56 +0900

    Initial commit

---
+++ linked_list.py
@@ -0,0 +1,49 @@
+class Node:
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.last_node = None
+
+    def to_list(self):
+        l = []
+        if self.head is None:
+            return l
+
+        node = self.head
+        while node:
+            l.append(node.data)
+            node = node.next_node
+        return l
+
+    def print_ll(self):
+        ll_string = ""
+        node = self.head
+        if node is None:
+            print(None)
+        while node:
+            ll_string += f" {str(node.data)} ->"
+            node = node.next_node
+
+        ll_string += " None"
+        print(ll_string)
+
+    def insert_beginning(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+            self.last_node = self.head
+
+        new_node = Node(data, self.head)
+        self.head = new_node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.insert_beginning(data)
+            return
+
+        self.last_node.next_node = Node(data, None)
+        self.last_node = self.last_node.next_node
--- server.py
+++ server.py
@@ -9,10 +9,8 @@ import linked_list
 # app
 app = Flask(__name__)
 
-# config
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
-
 
 # configure sqlite3 to enforce foreign key contraints
 @event.listens_for(Engine, "connect")
@@ -58,7 +56,7 @@ def create_user():
     )
     db.session.add(new_user)
     db.session.commit()
-    return jsonify({"message": "New user created!"}), 200
+    return jsonify({"message": "User created"}), 200
 
 
 @app.route("/user/descending_id", methods=["GET"])
@@ -67,105 +65,51 @@ def get_all_users_descending():
     all_users_ll = linked_list.LinkedList()
 
     for user in users:
-        all_users_ll.insert_at_begining(
+        all_users_ll.insert_beginning(
             {
                 "id": user.id,
                 "name": user.name,
                 "email": user.email,
-                "address": user.address,
+                "address": user.addres,
                 "phone": user.phone,
             }
         )
 
-    return jsonify(all_users_ll.to_array())
-
+    return jsonify(all_users_ll.to_list()), 200
 
 @app.route("/user/ascending_id", methods=["GET"])
 def get_all_users_ascending():
-    users = User.query.all()
-    all_users_ll = linked_list.LinkedList()
-
-    for user in users:
-        all_users_ll.insert_at_end(
-            {
-                "id": user.id,
-                "name": user.name,
-                "email": user.email,
-                "address": user.address,
-                "phone": user.phone,
-            }
-        )
-
-    return jsonify(all_users_ll.to_array())
+    pass
 
 
 @app.route("/user/<user_id>", methods=["GET"])
 def get_one_user(user_id):
-    users = User.query.all()
-
-    all_users_ll = linked_list.LinkedList()
-
-    for user in users:
-        all_users_ll.insert_at_begining(
-            {
-                "id": user.id,
-                "name": user.name,
-                "email": user.email,
-                "address": user.address,
-                "phone": user.phone,
-            }
-        )
-
-    user = all_users_ll.get_user_by_id(user_id)
-
-    return jsonify(user), 200
+    pass
 
 
-@app.route("/user/<user_id>", methods=["DELTE"])
+@app.route("/user/<user_id>", methods=["DELETE"])
 def delete_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
-    db.session.delete(user)
-    db.session.commit()
-    return ""
+    pass
 
 
 @app.route("/blog_post/<user_id>", methods=["POST"])
 def create_blog_post(user_id):
-    data = request.get_json()
-
-    user = User.query.filter_by(id=user_id).first()
-    if not user:
-        return jsonify({"message": "User does not exist!"}), 400
-
-    new_blog_post = BlogPost(
-        title=data["title"], body=data["body"], date=datetime.now(), user_id=user_id
-    )
-    db.session.add(new_blog_post)
-    db.session.commit()
-    return jsonify({"message": "New Blog Post created!"}), 200
+    pass
 
 
 @app.route("/user/<user_id>", methods=["GET"])
 def get_all_blog_posts(user_id):
-    print(BlogPost.query.filter_by(user_id=user_id))
-    return ""
+    pass
 
 
 @app.route("/blog_post/<blog_post_id>", methods=["GET"])
 def get_one_blog_post(blog_post_id):
-    post = BlogPost.query.filter_by(id=blog_post_id).first()
-    return ""
+    pass
 
 
-@app.route("/blog_post/<blog_post_id>", methods=["DELTE"])
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
 def delete_blog_post(blog_post_id):
-    post = BlogPost.query.filter_by(id=blog_post_id).first()
-    if not post:
-        return jsonify({"message": "no post"})
-
-    db.session.delete(post)
-    db.session.commit()
-    return ""
+    pass
 
 
 if __name__ == "__main__":
